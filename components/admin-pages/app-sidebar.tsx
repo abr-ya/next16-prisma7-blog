7f13c10ea5ef8ac9fefd3ee88c05965f32643749
abr-ya <gamer05@list.ru>
2026-01-01 21:20:53 +0300

    feat: Update Sidebar component

--- components/admin-pages/app-sidebar.tsx
+++ components/admin-pages/app-sidebar.tsx
@@ -1,3 +1,60 @@
-export const AppSidebar = () => {
-  return <div>app-sidebar</div>;
-};
+import { Calendar, Home, Inbox, Search } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Posts",
+    url: "/posts",
+    icon: Inbox,
+  },
+  {
+    title: "Categories",
+    url: "/categories",
+    icon: Calendar,
+  },
+  {
+    title: "Saved Posts",
+    url: "saved-posts",
+    icon: Search,
+  },
+];
+
+export const AppSidebar = () => (
+  <Sidebar>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>NextBlog</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+);
