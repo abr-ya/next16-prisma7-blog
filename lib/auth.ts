a2873097923eec3b858c53099d1331d4ba1435cd
abr-ya <gamer05@list.ru>
2026-01-01 18:45:55 +0300

    fix: Add baseURL to betterAuth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
