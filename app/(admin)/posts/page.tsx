a385052f479260cb558612621cba87b23be89e9d
abr-ya <gamer05@list.ru>
2026-01-19 22:18:22 +0300

    fix: Add requireAuth to Posts page

--- app/(admin)/posts/page.tsx
+++ app/(admin)/posts/page.tsx
@@ -1,11 +1,14 @@
 import { getAllPosts } from "@/app/_data/posts";
 import { Breadcrumbs } from "@/components/index";
 import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/lib/auth-utils";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
 const PostsPage = async () => {
+  await requireAuth();
+
   const data = await getAllPosts(); // todo: update it!
   console.log(data);
 
