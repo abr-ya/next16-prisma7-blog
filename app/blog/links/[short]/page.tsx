8bad1dfb2f6933efbf5314e1d1aab1034b02226c
abr-ya <gamer05@list.ru>
2026-01-31 13:18:57 +0300

    feat: Add some logs

--- app/blog/links/[short]/page.tsx
+++ app/blog/links/[short]/page.tsx
@@ -6,6 +6,7 @@ const BlogLinkPage = async ({ params }: { params: Promise<{ short: string }> }) 
     <div className="w-full flex flex-col items-center p-6 md:p-0">
       <div className="flex max-w-6xl flex-col gap-6 justify-center">
         <h1 className="text-2xl md:text-5xl font-semibold">{short}</h1>
+        <p className="text-muted-foreground">This page is under reconstruction.</p>
       </div>
     </div>
   );
--- components/blog-pages/link-block.tsx
+++ components/blog-pages/link-block.tsx
@@ -3,23 +3,24 @@
 import { IPostLink } from "@/app/_interfaces/post.interface";
 import Link from "next/link";
 import { Button } from "..";
+import { createLogEvent } from "@/app/_data/log";
 
 export const LinkBlock = ({ pl, userID }: { pl: IPostLink; userID: string }) => {
-  const goToHandler = () => {
-    console.log("User ID:", userID);
-    console.log("Link ID:", pl.linkId);
+  const goToHandler = async () => {
+    console.log("User ID:", userID, "Link ID:", pl.linkId);
+    await createLogEvent("goToLink", `User ${userID} opened link: ${pl.link.name}`);
     window.open(pl.link.url, "_blank");
   };
 
   return (
     <>
       <div className="flex self-center">{pl.link.name}</div>
+      <Button onClick={goToHandler} className="bg-green-300 text-green-900 hover:bg-green-200">
+        open link in new tab
+      </Button>
       <Link href={`/blog/links/${pl.link.shortCode}`} rel="noreferrer">
         <Button variant="outline">show link details</Button>
       </Link>
-      <Button variant="outline" onClick={goToHandler}>
-        open link in new tab
-      </Button>
     </>
   );
 };
