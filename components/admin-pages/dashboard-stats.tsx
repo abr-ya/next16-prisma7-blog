d3366702fe817674dc93922026b773cc021ad28c
abr-ya <gamer05@list.ru>
2026-02-13 08:31:52 +0300

    feat: Update DashboardStats component

--- components/admin-pages/dashboard-stats.tsx
+++ components/admin-pages/dashboard-stats.tsx
@@ -1,3 +1,6 @@
+import { Fragment } from "react/jsx-runtime";
+import { DashboardStatsCard } from "./dashboard-stats-card";
+
 interface DashboardStatsProps {
   totalPosts: number;
   totalCategories: number;
@@ -7,5 +10,19 @@ interface DashboardStatsProps {
 export const DashboardStats = ({ totalPosts, totalCategories, totalViews }: DashboardStatsProps) => {
   console.log("DashboardStats props:", { totalPosts, totalCategories, totalViews });
 
-  return <div>DashboardStats</div>;
+  const data = [
+    { title: "Total Posts", value: totalPosts },
+    { title: "Total Categories", value: totalCategories },
+    { title: "Total Views", value: totalViews },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full px-6">
+      {data.map((item) => (
+        <Fragment key={item.title}>
+          <DashboardStatsCard title={item.title} value={item.value} />
+        </Fragment>
+      ))}
+    </div>
+  );
 };
