378ab86afedd036923648b0b36768a72a02e917c
abr-ya <gamer05@list.ru>
2026-02-16 08:22:31 +0300

    feat: Update PostsTable component

--- components/admin-pages/posts-table.tsx
+++ components/admin-pages/posts-table.tsx
@@ -1,19 +1,49 @@
+"use client";
+
 import { Post } from "@/generated/prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
+import { Button, DataTable } from "..";
+import { ArrowUpDown } from "lucide-react";
 
 interface IPostsTableProps {
   data: Post[];
 }
 
+const columns: ColumnDef<Post>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Title
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <Link href={`/posts/${row.original.id}`} key={row.original.id}>
+        <h3 className="font-semibold">{row.original.title}</h3>
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => row.getValue("status"),
+  },
+  {
+    accessorKey: "views",
+    header: "Views",
+    cell: ({ row }) => row.getValue("views"),
+  },
+];
+
 export const PostsTable = ({ data }: IPostsTableProps) => {
   return (
     <div>
-      posts-table: {data.length}
-      {data.map((post) => (
-        <Link href={`/posts/${post.id}`} key={post.id}>
-          <h3 className="font-semibold">{post.title}</h3>
-        </Link>
-      ))}
+      posts count: {data.length}
+      <DataTable data={data} columns={columns} />
     </div>
   );
 };
